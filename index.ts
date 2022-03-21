3cb8c1a7471c34e8f81b3e6125313011ef48500d
xiexuan-star <xiexuan127@foxmail.com>
2022-03-22 01:24:28 +0800

    feat: complete promiseA+ until 2.2.7.4

--- index.ts
+++ index.ts
@@ -7,40 +7,87 @@ enum PROMISE_STATE {
 type FulfilledHandler<T> = (p: T) => any
 type RejectedHandler = (r: any) => any;
 
+type PromiseHandler<T> = (resolve: (value: T) => void, reject: (reason: any) => any) => any
+
 class PromiseMe<T extends any = any> {
   private state = PROMISE_STATE.PENDING;
   private value?: T;
   private reason: any;
+  private onFulfilledHandler: FulfilledHandler<T>[] = [];
+  private onRejectedHandler: RejectedHandler[] = [];
 
-  private onFulfilledHandler?: FulfilledHandler<T>;
-  private onRejectedHandler?: RejectedHandler;
-
-  private setFulfilled() {
-    if (!this.updateState(PROMISE_STATE.FULFILLED)) return;
-    this.onFulfilledHandler?.(this.value!);
+  constructor(handler: PromiseHandler<T>) {
+    try {
+      handler(this.setFulfilled.bind(this), this.setRejected.bind(this));
+    } catch (e: any) {
+      this.setRejected(e);
+    }
   }
 
-  private setRejected() {
+  private trigger() {
+    if (this.state === PROMISE_STATE.PENDING) return;
+    if (this.state === PROMISE_STATE.FULFILLED) {
+      this.onFulfilledHandler.forEach(cb => {
+        queueMicrotask(cb.bind(undefined, this.value!));
+      });
+    } else {
+      this.onRejectedHandler.forEach(cb => {
+        queueMicrotask(cb.bind(undefined, this.reason));
+      });
+    }
+    this.onRejectedHandler.length = 0;
+    this.onFulfilledHandler.length = 0;
+  }
+
+  private setFulfilled(value: T) {
+    if (!this.updateState(PROMISE_STATE.FULFILLED)) return;
+    this.value = value;
+    this.trigger();
+  }
+
+  private setRejected(reason: any) {
     if (!this.updateState(PROMISE_STATE.REJECTED)) return;
-    this.onRejectedHandler?.(this.reason);
+    this.reason = reason;
+    this.trigger();
   }
 
   private updateState(state: PROMISE_STATE) {
     if (this.state !== PROMISE_STATE.PENDING) return false;
     if ([PROMISE_STATE.FULFILLED, PROMISE_STATE.REJECTED].includes(state)) {
       this.state = state;
+      return true;
     }
-    return true;
+    return false;
   }
 
   then(onFulfilled?: (p: T) => any, onRejected?: (r: any) => any) {
     onFulfilled = onFulfilled || (v => v);
     onRejected = onRejected || (r => {throw r;});
-    if (typeof onFulfilled === 'function') {
-      this.onFulfilledHandler = onFulfilled;
+
+    const result = new PromiseMe((resolve, reject) => {
+        if (typeof onFulfilled === 'function') {
+          this.onFulfilledHandler.push((value: T) => {
+            try {
+              resolve(onFulfilled!(value));
+            } catch (e) {
+              reject(e);
+            }
+          });
+          if (typeof onRejected === 'function') {
+            this.onRejectedHandler.push((reason: any) => {
+              try {
+                resolve(onRejected!(reason));
+              } catch (e) {
+                reject(e);
+              }
+            });
+          }
+        }
+      }
+    );
+    if (this.state !== PROMISE_STATE.PENDING) {
+      this.trigger();
     }
-    if (typeof onRejected === 'function') {
-      this.onRejectedHandler = onRejected;
-    }
+    return result;
   }
 }
